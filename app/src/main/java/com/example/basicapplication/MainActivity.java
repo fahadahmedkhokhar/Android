83d85f15fdcf5be798147060ecbdb5084cb13253
fahadahmedkhokhar <33691176+fahadahmedkhokhar@users.noreply.github.com>
2019-07-27 21:07:45 +0500

    Calculator Application Designed with all functionality included

--- app/src/main/java/com/example/basicapplication/MainActivity.java
+++ app/src/main/java/com/example/basicapplication/MainActivity.java
@@ -3,12 +3,197 @@ package com.example.basicapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button zero, one, two, three, four, five, six,
+            seven, eight, nine, plus, subtract, divide,
+            multiply, dot, ac, equal;
+    TextView outputResult;
+
+    float mValueOne, mValueTwo;
+
+    boolean crunchifyAddition, mSubtract, crunchifyMultiplication, crunchifyDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputResult = (TextView) findViewById(R.id.display);
+        outputResult.setText("");
+        one = (Button) findViewById(R.id.one);
+        two = (Button) findViewById(R.id.two);
+        three = (Button) findViewById(R.id.three);
+        four = (Button) findViewById(R.id.four);
+        five = (Button) findViewById(R.id.five);
+        six = (Button) findViewById(R.id.six);
+        seven = (Button) findViewById(R.id.seven);
+        eight = (Button) findViewById(R.id.eight);
+        nine = (Button) findViewById(R.id.nine);
+        zero = (Button) findViewById(R.id.zero);
+        plus = (Button) findViewById(R.id.plus);
+        subtract = (Button) findViewById(R.id.minus);
+        divide = (Button) findViewById(R.id.divide);
+        multiply = (Button) findViewById(R.id.multiply);
+        ac = (Button) findViewById(R.id.ac);
+        equal = (Button) findViewById(R.id.equal);
+        dot = (Button)findViewById(R.id.dot);
+
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "1");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "2");
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "3");
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "4");
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "5");
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "6");
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "7");
+            }
+        });
+
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "8");
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "9");
+            }
+        });
+
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + "0");
+            }
+        });
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (outputResult == null) {
+                    outputResult.setText("");
+                } else {
+                    mValueOne = Float.parseFloat(outputResult.getText() + "");
+                    crunchifyAddition = true;
+                    outputResult.setText(null);
+                }
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(outputResult.getText() + "");
+                mSubtract = true;
+                outputResult.setText(null);
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(outputResult.getText() + "");
+                crunchifyMultiplication = true;
+                outputResult.setText(null);
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(outputResult.getText() + "");
+                crunchifyDivision = true;
+                outputResult.setText(null);
+            }
+        });
+
+        equal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(outputResult.getText() + "");
+
+                if (crunchifyAddition == true) {
+                    outputResult.setText(mValueOne + mValueTwo + "");
+                    crunchifyAddition = false;
+                }
+
+                if (mSubtract == true) {
+                    outputResult.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (crunchifyMultiplication == true) {
+                    outputResult.setText(mValueOne * mValueTwo + "");
+                    crunchifyMultiplication = false;
+                }
+
+                if (crunchifyDivision == true) {
+                    outputResult.setText(mValueOne / mValueTwo + "");
+                    crunchifyDivision = false;
+                }
+            }
+        });
+
+        ac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText("");
+            }
+        });
+
+        dot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                outputResult.setText(outputResult.getText() + ".");
+            }
+        });
     }
-}
+    }
